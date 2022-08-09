7e4de938bdc06f336893a87bf77d3785929c137f
Eldon Costa <eldoncosta1@gmail.com>
2022-08-09 10:24:25 -0300

    Update Dashboard.tsx

--- src/Dashboard.tsx
+++ src/Dashboard.tsx
@@ -2,8 +2,10 @@ import { Link } from "react-router-dom";
 
 export function Dashboard() {
   return (
-    <div className="min-h-full flex items-center justify-center p-16 px-4 sm:px-6 lg:px-8">
-      <Link to="/user">/Usuários</Link>
+    <div className="min-h-full flex flex-col items-center justify-center p-16 px-4 sm:px-6 lg:px-8">
+      <Link to="/user-list">Lista de usuários</Link>
+      <Link to="/user-create">Criar usuário</Link>
     </div>
   );
 }
+
