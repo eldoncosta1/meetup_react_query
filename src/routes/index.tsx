fed1797a7f85ed59fcc33bd12fd1fda755eb6f5b
Eldon Costa <eldoncosta1@gmail.com>
2022-08-09 10:23:06 -0300

    Update index.tsx

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,13 +2,15 @@ import { Routes as Switch, Route, Navigate } from "react-router-dom";
 
 import { Dashboard } from "../Dashboard";
 import { User } from "../User";
+import { UserCreate } from "../UserCreate";
 
 const Routes: React.FC = () => {
   return (
     <Switch>
       <Route path="/" element={<Dashboard />} />
-      <Route path="/user" element={<User />} />
-      <Route path="*" element={<Navigate replace to="/" />} />
+      <Route path="/user-list" element={<User />} />
+      <Route path="/user-create" element={<UserCreate />} />
+      {/* <Route path="*" element={<Navigate replace to="/" />} /> */}
     </Switch>
   );
 };
